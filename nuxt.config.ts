b9a7a89d8975d7b43438f8b445724e1114477e5d
권다애 <kwondarc@plani.co.kr>
2023-08-22 13:16:28 +0900

    commit

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,8 +2,5 @@
 export default defineNuxtConfig({
   devtools: { enabled: true },
   css : ["~/assets/styles/common.scss"],
-  app: {
-    baseURL: "/exam-uxui/",
-    buildAssetsDir: 'assets'
-  }
+  app: {}
 })

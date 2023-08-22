8699cd0dd19234382c8abb1a64ff18e72b179ef4
권다애 <kwondarc@plani.co.kr>
2023-08-22 12:43:11 +0900

    commit

--- pages/index.vue
+++ pages/index.vue
@@ -5,10 +5,10 @@
 <template>
   <ul>
     <li>
-      <nuxt-link to="/guide">가이드</nuxt-link>
+      <nuxt-link to="./guide">가이드</nuxt-link>
     </li>
     <li>
-      <nuxt-link to="/iphone14">폰</nuxt-link>
+      <nuxt-link to="./iphone14">폰</nuxt-link>
     </li>
   </ul>
 </template>
